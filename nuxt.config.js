59e592e483702161feeeafc842ad0aa5dafcda00
kui wang <kuiwang@wholeren.com>
2020-11-18 21:23:23 +0800

    nav表

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
-const path = require('path')
+const path = require('path');
 export default {
   server: {
     host: '0.0.0.0',
-    port: 3001, // default: 3000
+    port: 3001 // default: 3000
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -10,9 +10,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   loading: { color: '#3B8070' },
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -30,7 +30,7 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -39,12 +39,12 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     extend(config, ctx) {
-      config.resolve.alias['@'] = path.resolve(__dirname, './')
+      config.resolve.alias['@'] = path.resolve(__dirname, './');
 
       if (ctx.isClient && !ctx.isDev) {
-        config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
-        config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
-        config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+        config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+        config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true;
+        config.optimization.minimizer[0].options.terserOptions.compress.warnings = false;
       }
     },
     loaders: {
@@ -53,10 +53,10 @@ export default {
           javascriptEnabled: true,
           modifyVars: {
             'primary-color': '#4169e2',
-            'link-color': '#4169e2',
-          },
-        },
-      },
-    },
-  },
-}
+            'link-color': '#4169e2'
+          }
+        }
+      }
+    }
+  }
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="home">
-    <a-carousel autoplay>
-      <div v-for="item in myList" :key="item">
-        <h3>{{ item }}</h3>
-      </div>
-    </a-carousel>
-    <div class="pro-wrap">
-      <div class="pro-item" v-for="item in conList" :key="item.title">
-        <TopHeader :title="item.title"></TopHeader>
-        <div class="pro-list">
-          <a-card
-            hoverable
-            class="card"
-            v-for="pro in item.list"
-            :key="pro.title"
-          >
-            <img
-              slot="cover"
-              alt="example"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            />
-            <a-card-meta :title="pro.title">
-              <template slot="description">
-                {{ pro.des }}
-              </template>
-            </a-card-meta>
-          </a-card>
-        </div>
+    <div class="nav-bg">
+      <div class="nav-wrap">
+        <a-row class="nav-1">
+          <a-col :span="12"> 只学有用的</a-col>
+          <a-col :span="12" class="search">
+            <a-input-search placeholder="input search text" style="width: 200px" @search="onSearch" />
+          </a-col>
+        </a-row>
+        <a-row class="nav-2">
+          <span class="nav-title">方向：</span>
+          <div class="category-wrap">
+            <span
+              :class="{ active: categoryActive === index }"
+              v-for="(item, index) in categoryList"
+              :key="index"
+              @click="changeCategory(index)"
+            >
+              {{ item }}
+            </span>
+          </div>
+        </a-row>
       </div>
     </div>
   </div>
@@ -34,137 +28,94 @@
 
 <script>
 // @ is an alias to /src
-import TopHeader from '@/components/TopHeader.vue'
+import TopHeader from '@/components/TopHeader.vue';
+
 export default {
   name: 'Home',
   components: { TopHeader },
+  props: {},
   data() {
     return {
-      myList: [1, 2],
-      conList: [
-        {
-          title: 'vue',
-          list: [
-            {
-              title: 'vue教师管理系',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理系统3',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-          ],
-        },
-        {
-          title: 'react',
-          list: [
-            {
-              title: 'vue教师管理系',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理系统3',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-          ],
-        },
-        {
-          title: 'Node',
-          list: [
-            {
-              title: 'vue教师管理系',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师管理系统3',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-            {
-              title: 'vue教师系统4',
-              des: '使用vue+element实现教师管理',
-              img: require('@/assets/logo.png'),
-            },
-          ],
-        },
+      categoryList: [
+        '前端',
+        'JAVA',
+        'Python',
+        'Android',
+        'Go',
+        'PHP',
+        '测试',
+        '大数据',
+        '人工智能',
+        '数据结构和算法',
+        '数据库',
+        '微信小程序',
+        '运维'
       ],
-    }
+      categoryActive: 0,
+      myList: [1, 2]
+    };
   },
-}
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {
+    onSearch() {},
+    changeCategory(index) {
+      this.categoryActive = index;
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .home {
-  width: 1200px;
-  margin: 0 auto;
-  /deep/ .ant-carousel {
-    margin-top: 30px;
-    .slick-slide {
-      text-align: center;
-      height: 320px;
-      line-height: 160px;
-      background: #364d79;
-      overflow: hidden;
-    }
-  }
-  .pro-wrap {
-    .pro-item {
-      .pro-list {
+  min-height: 500px;
+  font-size: 20px;
+  background: #f9f9f9;
+  .nav-bg {
+    background: #fff;
+    .nav-wrap {
+      margin: 0 auto;
+      width: 1200px;
+      .nav-1 {
+        height: 100px;
+        display: flex;
+        align-items: center;
+        .search {
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-end;
+        }
+      }
+      .nav-2 {
         display: flex;
         flex-direction: row;
-        flex-wrap: wrap;
-        .card {
-          // width: 220px;
-          flex: 1;
-          padding: 20px;
+        align-items: center;
+        .nav-title {
+          color: #222;
+          font-weight: bold;
           margin-right: 20px;
-          &:nth-child(5) {
-            margin-right: 0;
+          font-size: 15px;
+        }
+        .category-wrap {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          align-items: center;
+          span {
+            font-size: 15px;
+            color: #333;
+            margin-right: 20px;
+            cursor: pointer;
+            padding: 3px 10px;
+            &:hover {
+              color: @primary-color;
+            }
+            &.active {
+              background: rgba(233, 173, 7, 0.2);
+              color: @primary-color;
+            }
           }
         }
       }
