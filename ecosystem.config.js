4fd215b738dac8d7ea1934fba8ce7bdfafe32619
kui wang <kuiwang@wholeren.com>
2020-11-18 17:03:53 +0800

    新的修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       instances: 'max',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+
       env: {
         NODE_ENV: 'development',
       },
